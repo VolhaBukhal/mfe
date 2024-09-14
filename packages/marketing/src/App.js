436c1aab01889e58ac66088384f6b80eee268b4c
Бухал Ольга Викторовна <OBukhal@alfabank.ru>
2024-09-14 11:30:28 +0300

    feat: add productionFrefix to css to avoid collision

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+// adding prefix to the short class name generated for production mode
+const generatedClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
 
 export default function App() {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generatedClassName}>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Landing />}></Route>
